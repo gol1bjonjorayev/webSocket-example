9ace09700272b5b1f28d2fdf5dddd21e6a442bb0
developer.jorayev <jorayevdev002@gmail.com>
2023-07-20 15:15:36 +0500

    first commit

---
+++ src/main/java/developer/jorayev/websocketexample/WebsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package developer.jorayev.websocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/developer/jorayev/websocketexample/WebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.jorayev.websocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
